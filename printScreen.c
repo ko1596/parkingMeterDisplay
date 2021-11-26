1afdbd834dacbbca239909d9ba7160733d6200e4
ko1596 <ko89022189@gmail.com>
2021-11-25 23:46:39 -0800

    bad verison

--- printScreen.c
+++ printScreen.c
@@ -1,98 +1,193 @@
-// #include <unistd.h>
-// #include <stdio.h>
-// #include <fcntl.h>
-// #include <linux/fb.h>
-// #include <sys/mman.h>
-
-
-// int main () {
-//  int fp=0;
-//  struct fb_var_screeninfo vinfo;
-//  struct fb_fix_screeninfo finfo;
-//  fp = open ("/dev/fb0",O_RDWR);
-
-
-//  if (fp < 0){
-//   printf("Error : Can not open framebuffer device/n");
-//   exit(1);
-//  }
-
-
-//  if (ioctl(fp,FBIOGET_FSCREENINFO,&finfo)){
-//   printf("Error reading fixed information/n");
-//   exit(2);
-//  }
- 
-//  if (ioctl(fp,FBIOGET_VSCREENINFO,&vinfo)){
-//   printf("Error reading variable information/n");
-//   exit(3);
-//  }
-
-
-//  printf("The mem is :%d\n",finfo.smem_len);
-//  printf("The line_length is :%d\n",finfo.line_length);
-//  printf("The xres is :%d\n",vinfo.xres);
-//  printf("The yres is :%d\n",vinfo.yres);
-//  printf("bits_per_pixel is :%d\n",vinfo.bits_per_pixel);
-//  close (fp);
-// }
-
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 #include <linux/fb.h>
 #include <sys/mman.h>
-
-
-int main () {
- int fp=0;
- struct fb_var_screeninfo vinfo;
- struct fb_fix_screeninfo finfo;
- long screensize=0;
- char *fbp = 0;
- int x = 0, y = 0;
- long location = 0;
- fp = open ("/dev/fb0",O_RDWR);
-
-
- if (fp < 0){
-  printf("Error : Can not open framebuffer device/n");
-  exit(1);
- }
-
-
- if (ioctl(fp,FBIOGET_FSCREENINFO,&finfo)){
-  printf("Error reading fixed information/n");
-  exit(2);
- }
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
  
- if (ioctl(fp,FBIOGET_VSCREENINFO,&vinfo)){
-  printf("Error reading variable information/n");
-  exit(3);
- }
-
-
-  screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
- /*這就是把fp所指的檔案中從開始到screensize大小的內容給映射出來，得到一個指向這塊空間的指標*/
- fbp =(char *) mmap (0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fp,0);
-   if ((int) fbp == -1)
-     {
-        printf ("Error: failed to map framebuffer device to memory./n");
-        exit (4);
-     }
-/*這是你想畫的點的位置座標,(0，0)點在螢幕左上角*/
-  x = 100;
-  y = 100;
-  location = x * (vinfo.bits_per_pixel / 8) + y  *  finfo.line_length;
-
-
-  *(fbp + location) = 100;  /* 藍色的色深 */  /*直接賦值來改變螢幕上某點的顏色*/
-  *(fbp + location + 1) = 15; /* 綠色的色深*/   
-  *(fbp + location + 2) = 200; /* 紅色的色深*/   
-  *(fbp + location + 3) = 0;  /* 是否透明*/  
-  munmap (fbp, screensize); /*解除對映*/
-  close (fp);    /*關閉檔案*/
-  return 0;
-
-
+ 
+//14byte文件头
+typedef struct
+{
+	char cfType[2];//文件类型，"BM"(0x4D42)
+	long cfSize;//文件大小（字节）
+	long cfReserved;//保留，值为0
+	long cfoffBits;//数据区相对于文件头的偏移量（字节）
+}__attribute__((packed)) BITMAPFILEHEADER;
+//__attribute__((packed))的作用是告诉编译器取消结构在编译过程中的优化对齐
+ 
+//40byte信息头
+typedef struct
+{
+	char ciSize[4];//BITMAPFILEHEADER所占的字节数
+	long ciWidth;//宽度
+	long ciHeight;//高度
+	char ciPlanes[2];//目标设备的位平面数，值为1
+	int ciBitCount;//每个像素的位数
+	char ciCompress[4];//压缩说明
+	char ciSizeImage[4];//用字节表示的图像大小，该数据必须是4的倍数
+	char ciXPelsPerMeter[4];//目标设备的水平像素数/米
+	char ciYPelsPerMeter[4];//目标设备的垂直像素数/米
+	char ciClrUsed[4]; //位图使用调色板的颜色数
+	char ciClrImportant[4]; //指定重要的颜色数，当该域的值等于颜色数时（或者等于0时），表示所有颜色都一样重要
+}__attribute__((packed)) BITMAPINFOHEADER;
+ 
+typedef struct
+{
+	unsigned short blue;
+	unsigned short green;
+	unsigned short red;
+	unsigned short reserved;
+}__attribute__((packed)) PIXEL;//颜色模式RGB
+ 
+BITMAPFILEHEADER FileHead;
+BITMAPINFOHEADER InfoHead;
+ 
+static char *fbp = 0;
+static int xres = 0;
+static int yres = 0;
+static int bits_per_pixel = 0;
+ 
+int show_bmp();
+ 
+int main ( int argc, char *argv[] )
+{
+	int fbfd = 0;
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	long int screensize = 0;
+	struct fb_bitfield red;
+	struct fb_bitfield green;
+	struct fb_bitfield blue;
+ 
+	//打开显示设备
+	fbfd = open("/dev/fb0", O_RDWR);
+	if (!fbfd)
+	{
+		printf("Error: cannot open framebuffer device.\n");
+		exit(1);
+	}
+ 
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo))
+	{
+		printf("Error：reading fixed information.\n");
+		exit(2);
+	}
+ 
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo))
+	{
+		printf("Error: reading variable information.\n");
+		exit(3);
+	}
+ 
+	printf("R:%d,G:%d,B:%d \n", vinfo.red, vinfo.green, vinfo.blue );
+ 
+	printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel );
+	xres = vinfo.xres;
+	yres = vinfo.yres;
+	bits_per_pixel = vinfo.bits_per_pixel;
+ 
+	//计算屏幕的总大小（字节）
+	screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+	printf("screensize=%d byte\n",screensize);
+ 
+	//对象映射
+	fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+	if ((int)fbp == -1)
+	{
+		printf("Error: failed to map framebuffer device to memory.\n");
+		exit(4);
+	}
+ 
+	printf("sizeof file header=%d\n", sizeof(BITMAPFILEHEADER));
+ 
+	printf("into show_bmp function\n");
+ 
+	//显示图像
+	show_bmp();
+ 
+	//删除对象映射
+	munmap(fbp, screensize);
+	close(fbfd);
+	return 0;
+}
+ 
+int show_bmp()
+{
+	FILE *fp;
+	int rc;
+	int line_x, line_y;
+	long int location = 0, BytesPerLine = 0;
+	char tmp[1024*10];
+	fp = fopen( "test.bmp", "rb" );
+  printf("opening test.bmp\n");
+	if (fp == NULL)
+	{
+		return( -1 );
+	}
+ 
+	rc = fread( &FileHead, sizeof(BITMAPFILEHEADER),1, fp );
+	if ( rc != 1)
+	{
+		printf("read header error!\n");
+		fclose( fp );
+		return( -2 );
+	}
+ 
+	//检测是否是bmp图像
+	if (memcmp(FileHead.cfType, "BM", 2) != 0)
+	{
+		printf("it's not a BMP file\n");
+		fclose( fp );
+		return( -3 );
+	}
+ 
+	rc = fread( (char *)&InfoHead, sizeof(BITMAPINFOHEADER),1, fp );
+	if ( rc != 1)
+	{
+		printf("read infoheader error!\n");
+		fclose( fp );
+		return( -4 );
+	}
+ 
+	//跳转的数据区
+	fseek(fp, FileHead.cfoffBits, SEEK_SET);
+	//每行字节数
+	BytesPerLine = (InfoHead.ciWidth * InfoHead.ciBitCount + 31) / 32 * 4;
+ 
+	line_x = line_y = 0;
+	//向framebuffer中写BMP图片
+  
+	while(!feof(fp))
+	{
+    printf("printf\n");
+		PIXEL pix;
+		unsigned short int tmp;
+		rc = fread( (char *)&pix, 1, sizeof(PIXEL), fp);
+		if (rc != sizeof(PIXEL))
+			break;
+		location = line_x * bits_per_pixel / 8 + (InfoHead.ciHeight - line_y - 1) * xres * bits_per_pixel / 8;
+ 
+		//显示每一个像素
+		*(fbp + location + 0)=pix.blue;
+		*(fbp + location + 1)=pix.green;
+		*(fbp + location + 2)=pix.red;
+		*(fbp + location + 3)=pix.reserved;
+    printf("%d\t%d\t%d\t\n",pix.blue, pix.green, pix.blue);
+ 
+		line_x++;
+		if (line_x == InfoHead.ciWidth )
+		{
+			line_x = 0;
+			line_y++;
+			if(line_y == InfoHead.ciHeight)
+				break;
+		}
+	}
+	fclose( fp );
+  printf("\n");
+	return( 0 );
 }
